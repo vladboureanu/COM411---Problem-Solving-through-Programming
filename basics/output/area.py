b119a7e9ba3204510e2ea4520815abbcaaf2be7c
repl.it user <replituser@example.com>
2021-03-12 10:26:26 +0000

    Week 3 python

---
+++ basics/output/area.py
@@ -0,0 +1,17 @@
+#Defining my own function to calculate area of a triangle 
+def calculate_area(h = 10,b = 5):
+  area = 0.5*b*h
+  return area
+
+def run():
+  x = calculate_area(4)
+  
+
+
+#Call for the function
+print(calculate_area())
+x = calculate_area(12,5)
+print(x+1)
+
+
+#Paramater - a value that you plug into a function
---
+++ basics/output/user_defined_functions.py
@@ -0,0 +1,7 @@
+def greet_user():
+  print("please eneter yourt name")
+  name=input()
+  print("Hello ", name)
+
+greet_user()
+greet_user()
