41c8af89a337cffd6df7bda8efc01edb74e87763
repl.it user <replituser@example.com>
2021-03-16 11:14:03 +0000

    Fruits

--- main.py
+++ main.py
@@ -1,48 +1,29 @@
-<<<<<<< HEAD
-def isPrime(x):
-  for i in range(2,x):
-    if x%i == 0:
-      return False
-  return True
+#Define an empty list
+fruits = []
 
-def findPrime(beginning, finish):
-  for j in range(beginning,finish):
-    if isPrime(j):
-      return j
+#Define a list with items
+vegetables = ["Carrot", "Peas"]
 
-def encrypt():
-  print("Provide two integers")
-  x = int(input())
-  y = int(input())
-  prime1 = findPrime(x,y)
-  print("Provide two integers")
-  x = int(input())
-  y = int(input())
-  prime2 = findPrime(x,y)
-  return prime1*prime2
+#Add items to the list
+fruits.append("Apple")
+fruits.append("Banana")
+fruits.append("Tomatoe")
+fruits.append("Banana")
 
-print(encrypt(), encrypt())
+print(fruits)
 
+#Remove an item from a list
+fruits.remove("Banana")
 
+print(fruits)
 
+#Remove item from index
+del fruits[1]
+print(fruits)
 
-#print("What is the number?")
-#x = int(input())
-#if isPrime(x):
- # print(f"The number {x} is prime!")
-#else:
-  #print("The number was not prime!")
-=======
-def display_box(name):
-    message = "* Hello {} *".format(name)
-    print("*" * len(message))
-    print(message)
-    print("*" * len(message))
+#Insert a value not at the end
+fruits.insert(1, "Pineapple")
+print(fruits)
 
-def greet_user():
-    print("Please enter your name")
-    name=input()
-    display_box(name)
-
-greet_user()
->>>>>>> c8daf372391857039f65a30bd821db8dd3e1433d
+#Access single element
+print(fruits[0])
