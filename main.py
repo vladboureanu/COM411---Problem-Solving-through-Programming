82d36e6cae3ac47426b7ce6696f844bb8f77d598
repl.it user <replituser@example.com>
2021-03-16 11:15:50 +0000

    Fruits

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-#Define an empty list
+
 fruits = []
 
 #Define a list with items
@@ -28,38 +27,3 @@ print(fruits)
 
 #Access single element
 print(fruits[0])
-=======
-def isPrime(x):
-  for i in range(2,x):
-    if x%i == 0:
-      return False
-  return True
-
-def findPrime(beginning, finish):
-  for j in range(beginning,finish):
-    if isPrime(j):
-      return j
-
-def encrypt():
-  print("Provide two integers")
-  x = int(input())
-  y = int(input())
-  prime1 = findPrime(x,y)
-  print("Provide two integers")
-  x = int(input())
-  y = int(input())
-  prime2 = findPrime(x,y)
-  return prime1*prime2
-
-print(encrypt(), encrypt())
-
-
-
-
-#print("What is the number?")
-#x = int(input())
-#if isPrime(x):
- # print(f"The number {x} is prime!")
-#else:
-  #print("The number was not prime!")
->>>>>>> 4317789c13f47f1b3a79937a2d1bf0116b3f87fa
