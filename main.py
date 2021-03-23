43479bf26c54d210e9581fcc9627750107d3a18f
repl.it user <replituser@example.com>
2021-03-23 10:31:01 +0000

    Students W5

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-def directions():
-    directions = ["Move forward", "Move Backward", "Turn Left", "Turn Right"]
-    return directions
+#Program that imitates a small database in the sense that it can:
+#insert new students and their mark
+#keep continually add students
+#print out student's name and their mark
+#avewrage mark of all students
+#find the maximum mark among all students
 
-def menu():
-    print("Please select a direction:")
-    dirs = directions()
-    for index in range(len(dirs)):
-        print("{}: {}".format(index, dirs[index]))
-    index = int(input())
-    return dirs[index]
 
-def run():
-    route =[]
-    print("working out escaping route...")
-    for count in range(5):
-        route.append(menu())
-    print(f"Escape route: {route}")
+def generate_stds():
+  print("Enter the name: ")
+  name = input()
+  print("Enter the grade: ")
+  grade = int(input())
+  return name, grade
 
-run()
+def all_stds():
+  all_students = []
+  while True:
+    all_students.append(generate_stds())
+    print("To stop adding students, type 0")
+    x = input()
+    if x == '0':
+      break
+  return all_students
+
+def print_students(lista):
+  for std in lista:
+    print(f"{std[0]} earned a grade {std[1]}")
+
+print_students(all_stds())
